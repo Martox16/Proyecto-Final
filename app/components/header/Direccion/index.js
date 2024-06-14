b1ffc874845a4fed715009f6a68448de17e8c0f4
Android Studio <devandroid@ort>
2024-06-14 09:07:29 -0300

    PASO API DIRE

--- app/components/header/Direccion/index.js
+++ app/components/header/Direccion/index.js
@@ -1,29 +1,56 @@
 'use client';
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './direccion.modules.css';
 
 export default function Direccion() {
     const [mostrarOpciones, setMostrarOpciones] = useState(false);
-    const [direccionSeleccionada, setDireccionSeleccionada] = useState("La Pampa 3270");
+    const [direccionSeleccionada, setDireccionSeleccionada] = useState("");
+    const [direcciones, setDirecciones] = useState([]);
 
-    const direcciones = ["La Pampa 3270", "Vallejos 4364", "Yatay 240","Rio 587", "Corrientes 546", "Agregar Dirección", "Editar Dirección"];
+    useEffect(() => {
+        const userId = '1'; // Supongamos que aquí tienes el ID de usuario
+        const apiUrl = `http://localhost:3000/direccion/${userId}`; // Reemplaza 'puerto' con el puerto donde se ejecutan las APIs
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            setDirecciones(data);
+            // Si hay direcciones cargadas, selecciona la primera por defecto
+            if (data.length > 0) {
+                setDireccionSeleccionada(data[0]);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }, []);
 
     const handleClick = (direccion) => {
         setDireccionSeleccionada(direccion);
         setMostrarOpciones(false);
     };
 
-    // Filtrar las direcciones para no mostrar la seleccionada
-    const direccionesFiltradas = direcciones.filter(direccion => direccion !== direccionSeleccionada);
+    // Función para manejar la acción de agregar dirección
+    const handleAgregarDireccion = () => {
+        // Aquí puedes manejar la lógica para agregar una nueva dirección
+    };
+
+    // Función para manejar la acción de editar dirección
+    const handleEditarDireccion = () => {
+        // Aquí puedes manejar la lógica para editar la dirección seleccionada
+    };
 
     return (
         <div className="direccion-container">
-            <button onClick={() => setMostrarOpciones(!mostrarOpciones)} className="Adress-button">
-                {direccionSeleccionada}
-            </button>
+            {direcciones.length === 0 && (
+                <button onClick={handleAgregarDireccion} className="Adress-button">
+                    Agregar Dirección
+                </button>
+            )}
+            {direcciones.length > 0 && (
+                <button onClick={() => setMostrarOpciones(!mostrarOpciones)} className="Adress-button">
+                    {direccionSeleccionada ? direccionSeleccionada : "Selecciona una dirección"}
+                </button>
+            )}
             <div className={`direccion-opciones ${mostrarOpciones ? 'show' : ''}`}>
-                {direccionesFiltradas.map((direccion, index) => (
+                {direcciones.map((direccion, index) => (
                     <button
                         key={index}
                         onClick={() => handleClick(direccion)}
