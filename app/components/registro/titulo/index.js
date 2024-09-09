1988c19450ff749f893dda65d13e8a340a18e354
Android Studio <devandroid@ort>
2024-09-09 08:48:14 -0300

    avances registro

---
+++ app/components/registro/titulo/index.js
@@ -0,0 +1,13 @@
+// components/iniciarSesion/titulo/index.js
+import React from 'react';
+import styles from './titulo.module.css';
+
+const Titulo = () => {
+    return (
+        <h1 className={styles.titulo}>
+            Registro
+        </h1>
+    );
+};
+
+export default Titulo;
--- app/view/registro/page.js
+++ app/view/registro/page.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import FlechaAtras from '../../../components/componentesGenerales/flechaAtras/index.js';
+import FlechaAtras from '../../components/componentesGenerales/flechaAtras/index.js';
 import styles from './registro.module.css';
+import Titulo from '../../components/registro/titulo/index.js';
+
 
 const Registro = () => {
     return (
         <div className={styles.RegistroContainer}>
             <div className={styles.topSection}></div>
+
             <div className={styles.flechaContainer}>
                 <FlechaAtras />
+            </div>
+
+            <div className={styles.contentContainer}>
+                <Titulo />
+
             </div>
         
         </div>
