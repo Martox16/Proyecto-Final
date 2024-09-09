e56f7cedbc345bddf515dae2b4471ff677891967
Martin <martucyg@gmail.com>
2024-09-08 23:21:07 -0300

    iniciarSesion Terminado

--- app/components/iniciarSesion/formulario/index.js
+++ app/components/iniciarSesion/formulario/index.js
@@ -1,12 +1,51 @@
-// components/iniciarSesion/formulario/index.js
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './formulario.module.css';
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`http://localhost:3000/iniciarSesion/${nombre}/${contrasena}`);
+            if (response.ok) {
+                const data = await response.json();
+                router.push('/view/home'); // Redirigir a la página de inicio si la autenticación es exitosa
+            } else if (response.status === 404) {
+                alert('Nombre no encontrado');
+            } else if (response.status === 401) {
+                alert('Contraseña incorrecta');
+            } else {
+                alert('Error en la autenticación');
+            }
+        } catch (error) {
+            console.error('Error al conectar con la API:', error);
+            alert('Error en la conexión');
+        }
+    };
+
     return (
-        <form className={styles.formulario}>
-            <input type="text" placeholder="Usuario" className={styles.input} />
-            <input type="password" placeholder="Contraseña" className={styles.input} />
+        <form className={styles.formulario} onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="Nombre" 
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                className={styles.input} 
+            />
+            <input 
+                type="password" 
+                placeholder="Contraseña" 
+                value={contrasena}
+                onChange={(e) => setContrasena(e.target.value)}
+                className={styles.input} 
+            />
             <div className={styles.extra}>
                 <a href="#" className={styles.olvidaste}>¿Olvidaste tu contraseña?</a>
                 <button type="submit" className={styles.boton}>Iniciar sesión</button>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Principal from './view/home/page'
+import Principal from './view/inicio/page'
 
 
 export default function Home() {
