3e2351beff60ac3fd059900672c39ec8c0b294f8
Android Studio <devandroid@ort>
2024-09-30 11:48:31 -0300

    p

--- app/view/crearResenia/page.js
+++ app/view/crearResenia/page.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import FlechaAtras from '../../components/componentesGenerales/flechaAtras/index.js';
+import styles from './crearResenia.module.css';
+
 
 const crearResenia = () => {
     return (
         <div>
             <FlechaAtras />
+            <div className={styles.contentContainer}>
+                {/* Tu contenido aquí */}
+            </div>
+            
         </div>
     );
 };
