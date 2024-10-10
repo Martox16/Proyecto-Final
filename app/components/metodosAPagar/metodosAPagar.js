05fd971408b11e8564eb21aed0e50b7e829fda91
Android Studio <devandroid@ort>
2024-10-10 08:52:59 -0300

    css de metodos

--- app/components/metodosAPagar/metodosAPagar.js
+++ app/components/metodosAPagar/metodosAPagar.js
@@ -6,9 +6,17 @@ const metodosAPagar = () => {
         <div className={styles.container}>
           <div className={styles.direccion}>
             <h2 className={styles.titulo}>Dirección de entrega</h2>
-            <p className={styles.ubicacion}>La Pampa 5370</p>
             <button className={styles.botonCambiar}>Cambiar</button>
           </div>
+          <div>
+          <p className={styles.ubicacion}>La Pampa 5370</p>
+          </div>
+        
+          <div className={styles.divLocal}>
+            <h2 className={styles.tituloLocal}>Nombre del local</h2>
+            <p className={styles.nombreLocal}>XXXXXXXXXX</p>
+          </div>
+          
     
           {/* Envío */}
           <div className={styles.envio}>
@@ -29,21 +37,26 @@ const metodosAPagar = () => {
           <div className={styles.metodoPago}>
             <h2 className={styles.titulo2}>Método De Pago</h2>
             <div className={styles.pagoOpciones}>
-            <button className={styles.botonEnvio}>
+            <button className={styles.botonPago}>
                 <img src="/precio.png" alt="Efectivo" className={styles.iconoEnvio} />
                 <p className={styles.p}>Efectivo</p>
             </button>
-            <button className={styles.botonEnvio}>
+            <button className={styles.botonPago}>
                 <img src="/tarjeta.png" alt="Tarjeta" className={styles.iconoEnvio} />
                 <p className={styles.p}>Tarjeta</p>
             </button>
-            <button className={styles.botonEnvio}>
+            <button className={styles.botonPago}>
                 <img src="/agregarTarjeta.png" alt="Agregar tarjeta" className={styles.iconoEnvio} />
-                <p className={styles.p}>+</p>
             </button>
             </div>
-            <img src="/cupon.png" alt="Cupón" className={styles.iconoEnvio} />
+
+            <div >
+            <div className={styles.cupon}> 
+            <img src="/cupon.png" alt="Cupón" className={styles.iconocupon} />
+            <p className={styles.p}>Aplicar cupón: </p>
+            </div>
             <input type="text" placeholder="Cupon:" className={styles.cuponInput} />
+          </div>
           </div>
     
           {/* Resumen */}
@@ -51,19 +64,19 @@ const metodosAPagar = () => {
             <h2 className={styles.titulo2}>Resumen</h2>
             <div className={styles.item}>
               <span>Costo de productos:</span>
-              <span>$24.000</span>
+              <span>XXXXX</span>
             </div>
             <div className={styles.item}>
               <span>Descuento:</span>
-              <span>$12.000</span>
+              <span>XXXXXX</span>
             </div>
             <div className={styles.item}>
               <span>Costo de envío:</span>
-              <span>$500</span>
+              <span>XXXXX</span>
             </div>
             <div className={styles.subtotal}>
               <span>Subtotal:</span>
-              <span>$12.500</span>
+              <span>XXXXXX</span>
             </div>
             
           </div>
